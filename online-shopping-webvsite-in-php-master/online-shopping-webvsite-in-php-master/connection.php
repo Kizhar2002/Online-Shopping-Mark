e65e82ead9a16bc84ec97951ff455a1181d845b3
Kizhar2002 <97729923+Kizhar2002@users.noreply.github.com>
2023-04-25 21:07:07 +0530

    Add files via upload

---
+++ online-shopping-webvsite-in-php-master/online-shopping-webvsite-in-php-master/connection.php
@@ -0,0 +1,4 @@
+<?php
+$con=mysqli_connect("database-1.cspilfowve69.us-east-1.rds.amazonaws.com","admin","admin123","store") or die(mysqli_error($con));
+//$con=mysqli_connect("localhost","root","","store") or die(mysqli_error($con));
+?>
